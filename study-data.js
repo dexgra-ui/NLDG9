66e162f4d8ddc2b095082745275cb7a3d805435c
dexgra-ui <dexgra@gmail.com>
2026-07-28 14:31:34 -0400

    Refresh study catalog for complete Women of Faith series

--- study-data.js
+++ study-data.js
@@ -1,10 +1,10 @@
 (function(){
   if(window.NLDG_LIBRARY)return;
   if(document.readyState==='loading'){
-    document.write('<script src="content-library.js?v=20260728-2"><\/script>');
+    document.write('<script src="content-library.js?v=20260728-3"><\/script>');
     return;
   }
   const script=document.createElement('script');
-  script.src='content-library.js?v=20260728-2';
+  script.src='content-library.js?v=20260728-3';
   document.head.appendChild(script);
 })();

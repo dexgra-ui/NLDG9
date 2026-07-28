6c10f772dcccfaa2ad0b602f67714d6479ed8b26
dexgra-ui <dexgra@gmail.com>
2026-07-28 10:39:19 -0400

    Refresh study library loader for Women of Faith expansion

--- study-data.js
+++ study-data.js
@@ -1,10 +1,10 @@
 (function(){
   if(window.NLDG_LIBRARY)return;
   if(document.readyState==='loading'){
-    document.write('<script src="content-library.js?v=20260728-1"><\/script>');
+    document.write('<script src="content-library.js?v=20260728-2"><\/script>');
     return;
   }
   const script=document.createElement('script');
-  script.src='content-library.js?v=20260728-1';
+  script.src='content-library.js?v=20260728-2';
   document.head.appendChild(script);
 })();

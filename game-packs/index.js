567627046a6e8ac94929bbd7780351e093f5ceb2
dexgra-ui <dexgra@gmail.com>
2026-08-06 07:00:48 -0400

    Register expanded General Bible pack

--- game-packs/index.js
+++ game-packs/index.js
@@ -1,1 +1,4 @@
-window.NLDG_GAME_PACK_INDEX={version:'1.0.0',packs:[{id:'general-bible',name:'General Bible',src:'game-packs/general-bible.js',status:'active',games:['scripture-or-suspicion','jeopardy','wheel','survey'],audiences:['kids','teens','adults','family','mixed']} ]};
+window.NLDG_GAME_PACK_INDEX={version:'1.1.0',packs:[
+{id:'general-bible',name:'General Bible',src:'game-packs/general-bible.js',status:'active',games:['scripture-or-suspicion','jeopardy','wheel','survey'],audiences:['kids','teens','adults','family','mixed']},
+{id:'general-bible-expanded',name:'General Bible Expanded',src:'game-packs/general-bible-expanded.js',status:'active',games:['scripture-or-suspicion','jeopardy','wheel','survey','finish-the-verse','who-am-i'],audiences:['kids','teens','adults','family','mixed']}
+]};
